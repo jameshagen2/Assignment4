ef8235b09ed25041207659fc756de927a71ee22f
Logan Johnson <logan.k.johnson@ndsu.edu>
2023-11-14 21:21:30 -0600

    This is commit #1. Created this Branch and added Members Page to the Project. It is not finished yet

---
+++ CSCI213_Assignment4/Members.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Members.aspx.cs" Inherits="WebApplication1.Members" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        Hello,
+        <asp:Label ID="lblFirstName" runat="server" Text="Label"></asp:Label>
+&nbsp;<asp:Label ID="lblLastName" runat="server" Text="Label"></asp:Label>
+    </p>
+    <p>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+</asp:Content>
---
+++ CSCI213_Assignment4/Members.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Members : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            /** Dont know how to implement this quite yet
+             * 
+            if (Session.Count != 0)
+            {
+                foreach (var item in Session.Keys)
+                {
+                    lblFirstName.Text = item + "   " + Session[item.ToString()];
+                    lblLastName.Text = item + "   " + Session[item.ToString()];
+                }
+            }
+            */
+
+            string conn = null;
+
+            KarateSchoolsDataContext dbcon = new KarateSchoolsDataContext(conn);
+
+            var result = from x in dbcon.Sections
+                         where(true) //need to fix
+                         select new
+                         {
+                             x.SectionName,
+                             x.Instructor_ID, //need to change to first and last
+                             x.SectionStartDate
+                             
+                             
+                         };
+            GridView1.DataSource = result;
+            GridView1.DataBind();
+
+
+        }
+    }
+}
